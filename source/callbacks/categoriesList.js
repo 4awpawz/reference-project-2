733e91d886b10e34318615345a6628ef3e8309d2
Jeff Schwartz <jefftschwartz@gmail.com>
2019-07-15 08:50:33 -0400

    wip

--- source/callbacks/categoriesList.js
+++ source/callbacks/categoriesList.js
@@ -1,6 +1,6 @@
 module.exports = ({ $tag, site }) => {
     site.categoriesCatalog.forEach(item =>
         $tag.append(/* html */ `
-            <a class="categories__list-item-link" href="${site.frags.find(frag => frag.matter.data.forCategory && frag.matter.data.forCategory[0] === item.category).url}">${item.category}</a>
+            <a class="blog-categories-list__item" href="${site.frags.find(frag => frag.matter.data.forCategory && frag.matter.data.forCategory[0] === item.category).url}">${item.category}</a>
     `));
 };
